ee82fd628346ffdd2aa9356a4355613eb798c3d4
rohancherukuri <rcherukuri10@gmail.com>
2026-03-21 15:30:56 -0500

    WIP

---
+++ llm-ai-stuff/data/match_cleaning.py
@@ -0,0 +1,17 @@
+import json
+import os
+import pandas as pd
+import pickle
+
+df = pd.DataFrame()
+
+for file in os.listdir("raw/matches"):
+    if not file.endswith(".json"):
+        continue
+    with open(os.path.join("raw/matches", file), "r") as f:
+        data = json.load(f)
+        df = pd.concat([df, pd.DataFrame([data])])
+
+with open("data/matches_df.pkl", "wb") as f:
+    pickle.dump(df, f)
+
